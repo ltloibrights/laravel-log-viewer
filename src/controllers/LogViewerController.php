490cae7dd485494e49353bac2ecd1dc2f5925edf
Le Trong Loi <ltloi@brights.vn>
2016-07-20 15:10:15 +0700

    Update LogViewerController.php

--- src/controllers/LogViewerController.php
+++ src/controllers/LogViewerController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Session;
 
 class LogViewerController extends BaseController
 {
